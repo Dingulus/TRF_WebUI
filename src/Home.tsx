558d0454c6b740395a6fc761f56ce39f72e70a90
Luan <llam3967@sdsu.edu>
2024-04-15 01:44:10 -0700

    Removed the download bit string button (not needed)

--- src/Home.tsx
+++ src/Home.tsx
@@ -84,26 +84,6 @@ export default function Home() {
     document.body.removeChild(a);
   };
 
-  // Function to convert button colors to a string
-  const generateGridStateString = () => {
-    let gridStateString = "";
-    for (let i = 0; i < numRows * numCols; i++) {
-      gridStateString += buttonColors[i] === "green" ? "1" : "0";
-    }
-    return gridStateString;
-  };
-
-  // Function to download the grid state as a text file
-  const downloadGridState = () => {
-    const gridStateString = generateGridStateString();
-    const element = document.createElement("a");
-    const file = new Blob([gridStateString], { type: "text/plain" });
-    element.href = URL.createObjectURL(file);
-    element.download = "grid_state.txt";
-    document.body.appendChild(element); // Required for this to work in Firefox
-    element.click();
-  };
-
   // Function to convert index to label (e.g., 0 -> A1, 1 -> A2, etc.)
   const indexToLabel = (index) => {
     const row = String.fromCharCode(65 + Math.floor(index / numCols));
@@ -160,9 +140,6 @@ export default function Home() {
           />
         </Col>
       </Row>
-      <Button className="custom-button" onClick={downloadGridState}>
-        Download Bit String
-      </Button>
       <Button className="custom-button" onClick={handleDownloadButtonClick}>
         Download Coordinates
       </Button>
